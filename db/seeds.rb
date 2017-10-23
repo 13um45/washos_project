0f768e93830a3da3347b66dc9de9151ce64f79b7
Christian Samuel <christian.leumas@icloud.com>
2017-10-23 00:39:41 -0400

    Seeds, gemfile, gemfile.lock and schema.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#default packages
+Package.create(
+  [
+    { name: 'Lavish', value: 2900 },
+    { name: 'Reveal', value: 7900 },
+    { name: 'Outright', value: 14900 }
+  ]
+)
+
+Option.create(
+  [
+    { name: 'Wax', value: 4000 },
+    { name: 'Leather conditioning', value: 3000 },
+    { name: 'Pet Hair removal', value: 2000 },
+    { name: 'Dress Plastic', value: 1500 },
+    { name: 'Paint Buffing', value: 10000 }
+  ]
+)
