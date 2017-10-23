23208c95334b913a71a9ae5d63dc5daccd23883f
Christian Samuel <christian.leumas@icloud.com>
2017-10-23 00:49:05 -0400

    fix bug where functions not running on page load.

--- app/assets/javascripts/appointments.js
+++ app/assets/javascripts/appointments.js
@@ -1,5 +1,9 @@
-$(document).on('ready page:load', function () {
+$(document).on('turbolinks:load', function () {
   $(".appointment_row").click(function () {
     window.location = $(this).data("link")
+  });
+
+  $('#app_date_time').datetimepicker({
+    format: 'Y-m-d H:i'
   });
 });
