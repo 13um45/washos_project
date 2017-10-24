72480c2979414fb9cef0ed7df3d5ca49d1575624
Christian Samuel <christian.leumas@icloud.com>
2017-10-24 09:03:31 -0400

    update params and remove un-needed methods.

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,14 +1,13 @@
 class AppointmentsController < ApplicationController
   def new
     @appointment = Appointment.new
+    @appointment.services.build
     @packages = Package.all
     @options = Option.all
   end
 
   def create
-    @appointment = Appointment.new(permit_params.except(:package, :options))
-    service = create_and_associate_service
-    @appointment.total_value = add_up_total_value(service)
+    @appointment = Appointment.new(appointment_params)
 
     if @appointment.save
       redirect_to @appointment
@@ -29,26 +28,31 @@ class AppointmentsController < ApplicationController
 
   private
 
-  def add_up_total_value(service)
-    sum = service.package.value
-    if service.options.present?
-      sum += service.options.pluck(:value).map(&:to_i).sum
-    end
-    sum
-  end
-
-  def create_and_associate_service
-    service = @appointment.build_service(package_id: params[:appointment][:package])
-    if params[:appointment][:options].present?
-      options = []
-      params[:appointment][:options].each { |_, v| options << v }
-      service.options = Option.find(options)
-    end
-    service.save
-    service
-  end
-
-  def permit_params
-    params.require(:appointment).permit(:customer_name, :appointment_time, :package, :options)
+  def appointment_params
+    params
+      .require(:appointment)
+      .permit(
+        :customer_name,
+        :appointment_time,
+        services_attributes:
+          [
+            :id,
+            :value,
+            :appointment_id,
+            :package_id,
+            :created_at,
+            :updated_at,
+            :_destroy,
+            service_options_attributes:
+              [
+                :id,
+                :service_id,
+                :option_id,
+                :created_at,
+                :updated_at,
+                :_destroy
+              ]
+          ]
+      )
   end
 end
